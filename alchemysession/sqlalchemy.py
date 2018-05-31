3aa3ddd0aa61b61f41bc3719bb581223a9c54198
Huy Hong <huyhong@gmail.com>
2018-05-31 09:00:21 +0200

    Change default table prefix

--- alchemysession/sqlalchemy.py
+++ alchemysession/sqlalchemy.py
@@ -15,7 +15,7 @@ LATEST_VERSION = 2
 
 
 class AlchemySessionContainer:
-    def __init__(self, engine=None, session=None, table_prefix="",
+    def __init__(self, engine=None, session=None, table_prefix="telethon",
                  table_base=None, manage_tables=True):
         if isinstance(engine, str):
             engine = sql.create_engine(engine)
